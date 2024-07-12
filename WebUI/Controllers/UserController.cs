00e9a9e750a4e3eeb35cdd549eb496d2796861d3
muhammetakkan <muhammettakkann@gmail.com>
2024-07-12 15:55:46 +0300

    createUser eklendi

---
+++ WebUI/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.ViewModels;
+
+namespace WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (IdentityError error in result.Errors) 
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
