a2bbf72e21da94de9973c893abab21f0bef34e3b
muhammetakkan <muhammettakkann@gmail.com>
2024-08-11 19:09:29 +0300

    rolesEdit

--- WebUI/Controllers/RolesController.cs
+++ WebUI/Controllers/RolesController.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.NamedPipes;
+using Microsoft.EntityFrameworkCore;
 using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class RolesController : Controller
     {
-        private RoleManager<AppRole> _roleManager;
-        public RolesController(RoleManager<AppRole> roleManager)
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
@@ -47,14 +50,61 @@ namespace WebUI.Controllers
             return View(roleModel);
         }
 
-        public IActionResult Edit(string id)
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
         {
-            if (id is null)
-                return View();
+            var role = await _roleManager.FindByIdAsync(id);
+            if (id is not null && role is not null && id == role.Id)
+            {
+                //ViewBag aracılığıyla Users ı taşıdık.
+                if(role is not null && role.Name is not null)
+                    ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                
+                return View(role);
+            }
+            return RedirectToAction("Index");
+        }
 
-            //burada kaldım.
+        //tekrar et, biraz ağır.
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, AppRole newRole)
+        {
+            if(id is not null && ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(id);
 
-            return View();
+                if(role is not null)
+                {
+                    role.Name = newRole.Name;
+                    var result = await _roleManager.UpdateAsync(role);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    foreach(var err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+
+                    //yazılan aşağıda
+                    /*
+                    if (role.Name != null)
+                        ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                    */
+                    //yazılan kod yukarıda
+
+                    ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                    if(ViewBag.Users is null || ViewBag.Users.Count() == 0)
+                    {
+                        ViewBag.Users = string.Empty;
+                    }
+                }
+            }
+            //sadee modelden gelen veriyi değil ayrıca Get teki ViewBag i burada sayfaya tekrar göndermemiz lazım.
+            
+            
+            return View(newRole);
         }
 
         [HttpPost]
